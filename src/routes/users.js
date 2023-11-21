360e658aeb525c55bba60b76229f94f5fcfb7e24
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-21 17:49:39 +0100

    add GET route to catch user attributes

--- src/routes/users.js
+++ src/routes/users.js
@@ -52,4 +52,29 @@ router.post('/attributes', authenticateApiKey, async (req, res) => {
   }
 });
 
+router.get('/attributes', authenticateApiKey, async (req, res) => {
+  try {
+    const { userTelegramID } = req.query;
+
+    if (!userTelegramID) {
+      return res.status(400).send({
+        msg: 'User Telegram ID is required.',
+      });
+    }
+
+    const db = await Database.getInstance();
+    const user = await db
+      .collection(USERS_COLLECTION)
+      .findOne({ userTelegramID });
+
+    return res.status(200).send({
+      userTelegramID,
+      attributes: user?.attributes,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: 'An error occurred', error });
+  }
+});
+
 export default router;
