813c086aa37216f2241b3c33970e5b0bf3de469d
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-27 21:44:21 +0100

    migration from js to ts

--- mocha.mocharc.cjs
+++ mocha.mocharc.cjs
@@ -1,9 +1,10 @@
-require('dotenv').config();
-
 module.exports = {
   timeout: 100000,
   exit: true,
-  require: 'src/test/hooks.js',
+  require: [
+    'ts-node/register', // Activer la compilation TypeScript au moment de l'exécution
+    './src/test/hooks.ts', // Spécifie le chemin vers le fichier de hooks
+  ],
   'async-only': true,
   retries: parseInt(process.env.RETRIES),
 };
