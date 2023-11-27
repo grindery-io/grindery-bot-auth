1ad05b6d92d1d10a34419bfb658e04cedd3ce644
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-27 21:48:18 +0100

    fix ci

--- mocha.mocharc.cjs
+++ mocha.mocharc.cjs
@@ -1,10 +1,7 @@
 module.exports = {
   timeout: 100000,
   exit: true,
-  require: [
-    'ts-node/register', // Activer la compilation TypeScript au moment de l'exécution
-    './src/test/hooks.ts', // Spécifie le chemin vers le fichier de hooks
-  ],
+  require: ['ts-node/register', './src/test/hooks.ts'],
   'async-only': true,
   retries: parseInt(process.env.RETRIES),
 };
