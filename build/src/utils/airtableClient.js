da4f99f28f56ae0ff74cbb5a9445ce06d78d7ed1
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-28 23:41:04 +0100

    Remove build directory from Git history

--- build/src/utils/airtableClient.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.base = void 0;
-const tslib_1 = require("tslib");
-const airtable_1 = tslib_1.__importDefault(require("airtable"));
-const secrets_1 = require("../../secrets");
-exports.base = new airtable_1.default({ apiKey: secrets_1.AIRTABLE_API_KEY }).base(secrets_1.AIRTABLE_BASE_ID);
-//# sourceMappingURL=airtableClient.js.map
