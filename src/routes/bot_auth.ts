b6e8bf2ad21cfe0e3e42856694729d0afd39f064
Thomas Coratger <thomas.coratger@gmail.com>
2023-11-28 15:27:25 +0100

    fix prettier

--- src/routes/bot_auth.ts
+++ src/routes/bot_auth.ts
@@ -21,7 +21,7 @@ router.get('/init', async (req, res) => {
         user_id: req.query.user_id || '',
         redirect_uri: req.query.redirect_uri || '',
       }),
-    ).toString("base64url");
+    ).toString('base64url');
     const currentDomain = `${req.protocol}://${req.get('host')}`;
     res.redirect(
       `https://orchestrator.grindery.org/oauth/authorize/?redirect_uri=${encodeURIComponent(
@@ -83,7 +83,9 @@ router.get('/callback', async (req, res) => {
       },
     );
 
-    const decodeState = JSON.parse(Buffer.from(String(req.query.state), "base64url").toString('utf-8'));
+    const decodeState = JSON.parse(
+      Buffer.from(String(req.query.state), 'base64url').toString('utf-8'),
+    );
 
     if (
       !userPropsResponse.data.result ||
