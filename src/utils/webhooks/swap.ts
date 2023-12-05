c7dcbbd5c3661e4abfd314007d2ee70c57bf1b14
Thomas Coratger <thomas.coratger@gmail.com>
2023-12-05 15:18:19 +0100

    improve doc

--- src/utils/webhooks/swap.ts
+++ src/utils/webhooks/swap.ts
@@ -10,13 +10,21 @@ import {
   updateUserOpHash,
 } from './utils';
 
+/**
+ * Handles the swap process based on provided parameters.
+ * @param {SwapParams} params - Parameters required for the swap.
+ * @returns {Promise<boolean>} - Promise resolving to a boolean indicating success or failure of the swap process.
+ */
 export async function handleSwap(params: SwapParams): Promise<boolean> {
+  // Get the database instance
   const db = await Database.getInstance();
 
+  // Fetch user information from the database based on the provided Telegram ID
   const userInformation = await db
     .collection(USERS_COLLECTION)
     .findOne({ userTelegramID: params.userTelegramID });
 
+  // If user information is not found, log an error and return true indicating handled status
   if (!userInformation) {
     console.error(
       `[SWAP EVENT] event id [${params.eventId}] User Telegram Id [${params.userTelegramID}] is not a user`,
@@ -24,9 +32,10 @@ export async function handleSwap(params: SwapParams): Promise<boolean> {
     return true;
   }
 
+  // Create a swap instance using createSwapTelegram function with provided parameters and user information
   const swap = await createSwapTelegram({ ...params, userInformation });
-  swap;
 
+  // If the swap status indicates success or failure, return true indicating handled status
   if (isSuccessfulTransaction(swap.status) || isFailedTransaction(swap.status))
     return true;
 
@@ -34,16 +43,17 @@ export async function handleSwap(params: SwapParams): Promise<boolean> {
 
   // Handle pending hash status
   if (isPendingTransactionHash(swap.status)) {
+    // Check if treatment duration for the swap is exceeded, if so, return true indicating handled status
     if (await isTreatmentDurationExceeded(swap)) return true;
 
-    // Check userOpHash and updateInDatabase for success
+    // If userOpHash is not available, mark the swap as successful and return true indicating handled status
     if (!swap.userOpHash)
       return (
         await swap.updateInDatabase(TRANSACTION_STATUS.SUCCESS, new Date()),
         true
       );
 
-    // Check status for userOpHash
+    // Check status for userOpHash and return the status if it's retrieved successfully or false if failed
     if ((tx = await swap.getStatus()) === true || tx == false) return tx;
   }
 
@@ -51,8 +61,9 @@ export async function handleSwap(params: SwapParams): Promise<boolean> {
   if (!tx && ((tx = await swap.swapTokens()) === true || tx == false))
     return tx;
 
-  // Finalize transaction handling
+  // Finalize transaction handling if tx data's txHash exists
   if (tx && tx.data.txHash) {
+    // Update transaction hash, update database, save to Segment and FlowXO
     updateTxHash(swap, tx.data.txHash);
     await Promise.all([
       swap.updateInDatabase(TRANSACTION_STATUS.SUCCESS, new Date()),
@@ -64,17 +75,19 @@ export async function handleSwap(params: SwapParams): Promise<boolean> {
       ),
     );
 
+    // Log successful swap event completion
     console.log(
-      `[${swap.txHash}] swap event [${tx.data.txHash}] from ${params.userTelegramID} finished finished.`,
+      `[${swap.txHash}] swap event [${tx.data.txHash}] from ${params.userTelegramID} finished.`,
     );
     return true;
   }
 
-  // Handle pending hash for userOpHash
+  // Handle pending hash for userOpHash, if available
   tx &&
     tx.data.userOpHash &&
     updateUserOpHash(swap, tx.data.userOpHash) &&
     (await swap.updateInDatabase(TRANSACTION_STATUS.PENDING_HASH, null));
 
+  // Return false indicating swap process is not fully handled
   return false;
 }
